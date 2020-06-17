fa1c8f41b2a9e7fc84cd7a307703aa042f6e5116
Alexey Churov <churov_lesha_099@bk.ru>
2020-06-18 01:51:55 +0300

    added admin's controllers

---
+++ src/main/java/by/devincubator/services/user/exceptions/UnknownCreationException.java
@@ -0,0 +1,34 @@
+package by.devincubator.services.user.exceptions;
+
+/**
+ * {@code UnknownCreationException} is an unchecked exception,
+ * can be thrown when something wrong happened during
+ * {@Question}, {@Test} or {@Topic} creation.
+ *
+ * @Author Nikita Muzyka
+ */
+
+public class UnknownCreationException extends RuntimeException {
+
+    static final long serialVersionUID = -2443950159561908509L;
+
+    public UnknownCreationException() {
+        super();
+    }
+
+    public UnknownCreationException(String message) {
+        super(message);
+    }
+
+    public UnknownCreationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UnknownCreationException(Throwable cause) {
+        super(cause);
+    }
+
+    protected UnknownCreationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
