51e02c93401a66502d8b9d1715e6665e95f80f0e
Nik <nikita_muzyka_93@icloud.com>
2020-07-06 00:49:04 +0300

    small update

--- src/main/java/by/devincubator/dits/services/general/implementations/UserServiceImpl.java
+++ src/main/java/by/devincubator/dits/services/general/implementations/UserServiceImpl.java
@@ -29,7 +29,6 @@ public class UserServiceImpl implements UserService {
     private static final boolean IS_NOT_AUTHORISED = false;
     private static final boolean IS_AUTHORISED = true;
     private static final UserInfoDTO GUEST = new UserInfoDTO("guest", new LinkedList<>(), IS_NOT_AUTHORISED);
-    private static final boolean IS_AUTHORISED = true;
     private UserRepository userRepository;
     private RoleRepository roleRepository;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
